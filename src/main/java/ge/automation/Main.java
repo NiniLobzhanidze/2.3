e502942a442263b6fdfdf204bc583566314ec22c
NiniLobzhanidze <lobzhanidze264@gmail.com>
2024-12-15 21:01:24 +0400

    2.3

---
+++ src/main/java/ge/automation/Main.java
@@ -0,0 +1,14 @@
+package ge.automation;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {18, -3, 5, -7, 6, 89, 13, 0, -89};
+        for (int i=0; i < arr.length -1; i++){
+        int min = i;
+        System.out.println(i);
+
+
+
+        }
+    }
+}
